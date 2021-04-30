c745173399055c70622604aae22b9287ac1abf15
serkviseal <serkviseal15@gmail.com>
2021-05-01 02:33:02 +0300

    circla avatar ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(children: [
+          CircleAvatar(
+            radius: 50.0,
+            backgroundColor: Colors.teal[300],
+            backgroundImage: AssetImage('images/profile.png'),
+          ),
+          Text(
+            'Kimenyi M. Serge',
+            style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold ),
+          )
+        ])),
       ),
-    ),
-  );
+    );
+  }
 }

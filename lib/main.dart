48fd91ad52e45465d91d5f6a6951a3e834f05263
serkviseal <serkviseal15@gmail.com>
2021-05-01 05:42:54 +0300

    added card & listtile to ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,112 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(children: [
-          CircleAvatar(
-            radius: 50.0,
-            backgroundColor: Colors.teal[300],
-            backgroundImage: AssetImage('images/profile.png'),
-          ),
-          Text(
-            'Kimenyi M. Serge',
-            style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold ),
-          )
-        ])),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.teal[300],
+              backgroundImage: AssetImage('images/profile.png'),
+            ),
+            Text(
+              'Kimenyi M. Serge',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'MOBILE & WEB DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20.0,
+                color: Colors.teal[100],
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[200],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.code,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'github.com/serkviseal',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.contact_phone,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+250 *** *** ***',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '*******@yahoo.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.location_on,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'Kigali, Rwanda',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
